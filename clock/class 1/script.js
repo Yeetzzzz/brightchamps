7566f085102f879c4f6bf5a0f07f99815b24d204
BuildTools <unconfigured@null.spigotmc.org>
2024-02-05 17:56:09 +0700

    idk

--- clock/class 1/script.js
+++ clock/class 1/script.js
@@ -1,4 +1,22 @@
-let i = 6;
-i = 9;
-console.log(i/3);
-console.log(i === 3);
+const time = document.getElementById("time");
+const ampm = document.getElementById("ampm");
+const information = document.getElementById("info");
+const title = document.getElementsByTagName("h1")[0];
+
+function changeToNight() {
+    time.style.color = "var(--time-night)";
+    ampm.style.color = "var(--ampm-night)";
+    information.style.color = "var(--text-night)";
+    title.style.color = "var(--text-night)";
+    document.body.style.backgroundColor = "var(--bg-night)";
+}
+
+function changeToDay() {
+    time.style.color = "var(--time-day)";
+    ampm.style.color = "var(--ampm-day)";
+    information.style.color = "var(--text-day)";
+    title.style.color = "var(--text-day)";
+    document.body.style.backgroundColor = "var(--bg-day)";
+}
+
+changeToNight()
